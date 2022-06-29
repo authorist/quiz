77cbc95a9555f3886d907ebd9cf5d2a6e1230752
authorist <salihtekin0001@gmail.com>
2022-06-29 12:00:18 +0300

    quiz migrationı oluşturuldu Factory seed oluşturuldu Seed yapısı tablolara bölündü.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,7 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::insert([
+        //     'name'=>'Salih Tekin',
+        //     'email'=>'st@st',
+        //     'email_verified_at' => now(),
+        //     'type'=>'admin',
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10),
+        // ]);         bunuda UserSeeder a ekledik artık buraya gerek yok
+//her seede ayrı çagıracagız aşagıdaki thiscall ile
+                $this->call([
+                        UserSeeder::class,
+                        QuizSeeder::class,
+                ]);
+        // \App\Models\User::factory(5)->create(); //bu şekilde herkes kendi seedini çagırır çalıştırır hepsi bu sayfada değil yani herkes kendi seed bu user ın seedi
+         //buradan kesip Userseeder a QuizSeeder a ekledik
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
